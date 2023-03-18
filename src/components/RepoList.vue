cf6d5b45de8242c15e9296de93514ff3f42840c7
Sunday Famous <sundayfamous25@yahoo.com>
2023-03-18 23:03:39 +0100

    24

--- src/components/RepoList.vue
+++ src/components/RepoList.vue
@@ -8,16 +8,38 @@
    
 </template>
 
-<script>
+<script setup>
+import { ref } from "vue";
+const reposData = ref([]);
+const loading = ref(true);
+const getRepo = () => {
+  let url = " https://api.github.com/users/kperusi/repos";
+  fetch(url)
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      reposData.value = data;
+      console.log(".....", data);
+    })
+    .catch((error) => {
+      this.error = error;
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+};
+getRepo();
 
-export default {
-    data(){
-        return{
+
+// export default {
+//     data(){
+//         return{
             
-        }
-    }
+//         }
+//     }
 
-}
+// }
 </script>
 
 
